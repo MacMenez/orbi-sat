da5f97eeb80d5e47fd2288f70c30aaf7416ec771
David Ram Menez <dcm.facul@gmail.com>
2026-05-30 23:57:17 -0300

    Código-fonte de Integração SQL + ESP32 + Python

---
+++ orbi_projetos/satelite.py
@@ -0,0 +1,9 @@
+# Conexão ao Banco de Dados
+
+# Variáveis globais para armazenar as últimas leituras recebidas
+
+# Função de callback para conexão MQTT
+
+# Função de callback para mensagens MQTT
+
+# a função abaixo manipula trafego de rede, trata callbacks e manipula reconexões.
